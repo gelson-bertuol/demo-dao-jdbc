f6cf4cd792360d49bf0be158184873f7033bad44
Gelson <gelson.bertuol@gmail.com>
2022-06-12 19:18:34 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -43,7 +46,6 @@ public class SellerDaoJDBC implements SellerDao {
 
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
 		String strSQL = 
 			"SELECT seller.*, department.Name as DepName " +
 			"FROM seller INNER JOIN department ON seller.DepartmentId = department.Id " +
@@ -58,6 +60,40 @@ public class SellerDaoJDBC implements SellerDao {
 				return obj;
 			}
 			return null;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		String strSQL = 
+			"SELECT seller.*, department.Name as DepName " +
+			"FROM seller INNER JOIN department ON seller.DepartmentId = department.Id " +
+			"WHERE DepartmentId = ? ORDER BY Name";
+		try {
+			st = conn.prepareStatement(strSQL);
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();
+			List<Seller> sellerList = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while (rs.next()) {
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(dep.getId(), dep);
+				}
+				Seller obj = instantiateSeller(rs, dep);
+				sellerList.add(obj);
+			}
+			return sellerList;
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
 		} finally {
